322b87f2cd922bc6df4b591a82dd4f38cbdbbb29
Stanley REGNARD <stanley.regnard@gmail.com>
2019-11-03 15:59:07 +0100

    bool

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 14:49:24 by sregnard          #+#    #+#             */
-/*   Updated: 2019/10/23 18:29:54 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/11/03 14:44:34 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
+# include <stdbool.h>
 # include "list.h"
 # include "get_next_line.h"
 # include "read_file.h"
