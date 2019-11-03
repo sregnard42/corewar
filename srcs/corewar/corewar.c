7d83b5c40131464ac2087225ad72ed32b72c55a6
Stanley REGNARD <stanley.regnard@gmail.com>
2019-11-03 15:00:50 +0100

    example ft_error

--- srcs/corewar/corewar.c
+++ srcs/corewar/corewar.c
@@ -6,13 +6,24 @@
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/27 20:15:20 by sregnard          #+#    #+#             */
-/*   Updated: 2019/10/27 20:27:24 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/11/03 15:00:06 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
+/*
+static void free_all(void *vm)
+{
+	vm += 0;
+	ft_printf("free all\n");
+	return ;
+}
+	ft_error((void *)&vm, &free_all, "Trigger error\n");
+*/
+
 int main()
 {
+//	t_vm	vm;
 	write(1, "corewar\n", 8);
 }
