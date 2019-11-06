f3ac3f0fad42b09ff138efd1b7a824aa6751fe75
sregnard <sregnard@student.42.fr>
2019-11-06 15:05:35 +0100

    transferred print_hex to libft as ft_hexdump

--- srcs/corewar/parsing.c
+++ srcs/corewar/parsing.c
@@ -6,43 +6,24 @@
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 12:44:20 by sregnard          #+#    #+#             */
-/*   Updated: 2019/11/06 14:15:40 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/11/06 15:01:51 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-static void		print_hex(char *content, int size)
-{
-	int	i;
-	int index;
-
-	index = 0;
-	while (index < size)
-	{
-		i = 0;
-		while (i++ < 16 && index < size)
-			ft_printf("%02x ", (unsigned char)content[index++]);
-		ft_putln();
-	}
-}
-
 static void		parse_file(t_vm *vm, char *file)
 {
-	char	content[CHAMP_MAX_SIZE + 1];
+	char	content[BUFF_SIZE + 1];
 	int		fd;
 	int		ret;
 
 	(fd = open(file, O_RDONLY)) == -1 ? 
 	ft_error(vm, &free_all, "ERROR: file doesn't exist\n") : 0;
-	ret = read(fd, content, CHAMP_MAX_SIZE);
-	ft_printf("File size : %dB\n", ret);
+	ret = read(fd, content, BUFF_SIZE);
 	ret < 1 ? ft_error(vm, &free_all, "ERROR: empty file\n") : 0;
 	content[ret] = '\0';
-	ft_putendl("\nText brut :");
-	write(1, content, ret);
-	ft_putendl("\nText hexdump :");
-	print_hex(content, ret);
+	ft_hexdump(content, ret);
 }
 
 void		parse_args(t_vm *vm, int ac, char **av)
