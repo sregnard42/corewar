5d20e6e168e83011e810a061a501ff33b632f0d4
sregnard <sregnard@student.42.fr>
2019-11-10 16:04:34 +0100

    norm

--- libft/srcs/ft_mem/ft_memrev.c
+++ libft/srcs/ft_mem/ft_memrev.c
@@ -6,7 +6,7 @@
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/10 12:47:17 by sregnard          #+#    #+#             */
-/*   Updated: 2019/11/10 13:31:14 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/11/10 14:47:29 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,10 @@
 void		ft_memrev(void *p, size_t n)
 {
 	unsigned char	*c;
-//	unsigned char	swap;
 	size_t			i;
 
 	c = (unsigned char *)p;
 	i = 0;
-	ft_printf("ft_memrev:\n");
 	while (i < n)
-	{
-		ft_printf("c[%d] = %02x\n", i, c[i]);
-		++i;
-	}
-	ft_putln();
-	i = 0;
-	while (i < n)
-	{
-		ft_printf("Before\tc[%d] = %02x & c[%d] = %02x\n", i, c[i], n - 1, c[n - 1]);
-		ft_memswap(c + i, c + n - 1, sizeof(unsigned char));
-		/*
-		swap = c[i];
-		c[i] = c[n - 1];
-		c[n - 1] = swap;
-		*/
-		ft_printf("After\tc[%d] = %02x & c[%d] = %02x\n", i, c[i], n - 1, c[n - 1]);
-		++i;
-		--n;
-	}
+		ft_memswap(c + i++, c + n-- - 1, sizeof(unsigned char));
 }
