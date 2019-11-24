59775060dc19f3c25d1c909088a8852d3531f1b9
Christelle Mouele <christelle.mouele@gmail.com>
2019-11-24 15:50:20 +0100

    Fix op_sti() function

--- srcs/corewar/instructions/st.c
+++ srcs/corewar/instructions/st.c
@@ -13,6 +13,24 @@
 #include "corewar.h"
 
 /*
+**      Takes a register and a register / indirect. Copies the value of the
+**      first argument to the address of the second argument.
+*/
+
+void    store(t_vm *vm, unsigned int src, unsigned int dst)
+{
+    t_process		*proc;
+
+    proc = vm->procs.cur;
+    ft_memcpy(&(vm->arena[dst]), &(proc->reg[src]), REG_SIZE); //ft_arenacpy
+    if (vm->verbose & V_OPERATIONS)
+    {
+        ft_printf("Player %d \"%s\" ", proc->champ->id, proc->champ->name);
+        ft_printf("stored value %u in address %u\n", proc->reg[src], dst);
+    }
+}
+
+/*
 **		Takes a register and a register / indirect. Copies the value of the
 **		first argument in the second argument.
 */
@@ -48,26 +66,24 @@ void	op_st(void *vm_ptr)
 void	op_sti(void *vm_ptr)
 {
     t_vm			*vm;
+    t_process		*proc;
 	t_args			*args;
-	t_process		*proc;
 	unsigned int	src;
 	unsigned int	dst[2];
-	unsigned int 	dst_sum;
 
 	vm = (t_vm *)vm_ptr;
+    proc = vm->procs.cur;
 	args = &vm->procs.cur->args;
-	proc = vm->procs.cur;
 	src = args->first->val;
-	dst[0] = args->first->next->val;
-	dst[1] = args->first->next->next->val;
-	dst_sum = dst[0] + dst[1];
-
+	if (args->first->next->type == REG_CODE)
+        ft_memcpy(&dst[0], &(proc->reg[args->first->next->val]), REG_SIZE);
+    else
+        dst[0] = args->first->next->val;
+    if (args->first->next->next->type == REG_CODE)
+        ft_memcpy(&dst[1], &(proc->reg[args->first->next->next->val]), REG_SIZE);
+	else
+        dst[1] = args->first->next->next->val;
 	if (vm->verbose & V_OPERATIONS)
 		ft_printf("sti %u, %u, %u | ", src, dst[0], dst[1]);
-	ft_memcpy(&dst_sum, proc->reg[src], REG_SIZE);
-	if (vm->verbose & V_OPERATIONS)
-	{
-		ft_printf("Player %d \"%s\" ", proc->champ->id, proc->champ->name);
-		ft_printf("loaded value %u in address %u\n", src, dst);
-	}
+	store(vm, src, vm->pc + ((dst[0] + dst[1]) % IDX_MOD));
 }
