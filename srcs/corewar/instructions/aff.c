bb65e2af4819ce57cf81939b3648611a5016fbed
Christelle Mouele <christelle.mouele@gmail.com>
2019-12-01 13:59:08 +0100

    Add op_aff() instruction

--- srcs/corewar/instructions/aff.c
+++ srcs/corewar/instructions/aff.c
@@ -6,18 +6,24 @@
 /*   By: cmouele <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/17 12:12:47 by cmouele           #+#    #+#             */
-/*   Updated: 2019/11/17 12:12:48 by cmouele          ###   ########.fr       */
+/*   Updated: 2019/12/01 13:58:50 by cmouele          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
 /*
-**		Comment
+**		Takes a register as an ASCII, and displays it on the standard output.
 */
 
 void	op_aff(void *vm_ptr)
 {
-    vm_ptr += 0;
-    return ;
+    t_vm			*vm;
+    t_args          *args;
+    unsigned int    val;
+
+    vm = (t_vm *)vm_ptr;
+    args = &vm->procs.cur->args;
+    val = args->first->val % 256;
+    vm->print("%c\n", val);
 }
