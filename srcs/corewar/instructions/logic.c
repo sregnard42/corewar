8beeea49598c0bfeecf5d96f75c5d18fc89614a5
Christelle Mouele <christelle.mouele@gmail.com>
2019-12-01 15:26:13 +0100

    Add op_and(), op_or() and op_xor() functions

--- srcs/corewar/instructions/logic.c
+++ srcs/corewar/instructions/logic.c
@@ -13,31 +13,115 @@
 #include "corewar.h"
 
 /*
-**		Comment
+**      Stores the result of the logical operations in a register.
+*/
+
+void    logic(t_vm *vm, unsigned int dst, unsigned int val)
+{
+    t_process		*proc;
+
+    proc = vm->procs.cur;
+    proc->carry = (val == 0);
+    ft_memcpy(&proc->reg[dst], &val, sizeof(t_reg));
+    if (vm->verbose & V_OPERATIONS)
+    {
+        vm->print("Player %d \"%s\" ", proc->champ->id, proc->champ->name);
+        vm->print("stored value %u in register %u\n", val, dst);
+    }
+}
+
+/*
+**		Takes two registers / directs / indirects, applies a logical AND
+**      between the two, and stores the result in the third parameter that is a
+**      register. Modifies the carry.
 */
 
 void	op_and(void *vm_ptr)
 {
-    vm_ptr += 0;
-    return ;
+    t_vm            *vm;
+    t_process		*proc;
+    t_args			*args;
+    unsigned int    src[2];
+    unsigned int    dst;
+
+    vm = (t_vm *)vm_ptr;
+    proc = vm->procs.cur;
+    args = &vm->procs.cur->args;
+    ft_bzero(&src, sizeof(src));
+    if (args->first->type == REG_CODE)
+        ft_memcpy(&src[0], &(proc->reg[args->first->val]), REG_SIZE);
+    else
+        src[0] = args->first->val;
+    if (args->first->next->type == REG_CODE)
+        ft_memcpy(&src[1], &(proc->reg[args->first->next->val]), REG_SIZE);
+    else
+        src[1] = args->first->next->val;
+    dst = args->first->next->next->val;
+    if (vm->verbose & V_OPERATIONS)
+        vm->print("and %u, %u, %u | ", src[0], src[1], dst);
+    logic(vm, dst, src[0] & src[1]);
 }
 
 /*
-**		Comment
+**		Takes two registers / directs / indirects, applies a logical OR
+**      between the two, and stores the result in the third parameter that is a
+**      register. Modifies the carry.
 */
 
 void	op_or(void *vm_ptr)
 {
-    vm_ptr += 0;
-    return ;
+    t_vm            *vm;
+    t_process		*proc;
+    t_args			*args;
+    unsigned int    src[2];
+    unsigned int    dst;
+
+    vm = (t_vm *)vm_ptr;
+    proc = vm->procs.cur;
+    args = &vm->procs.cur->args;
+    ft_bzero(&src, sizeof(src));
+    if (args->first->type == REG_CODE)
+        ft_memcpy(&src[0], &(proc->reg[args->first->val]), REG_SIZE);
+    else
+        src[0] = args->first->val;
+    if (args->first->next->type == REG_CODE)
+        ft_memcpy(&src[1], &(proc->reg[args->first->next->val]), REG_SIZE);
+    else
+        src[1] = args->first->next->val;
+    dst = args->first->next->next->val;
+    if (vm->verbose & V_OPERATIONS)
+        vm->print("and %u, %u, %u | ", src[0], src[1], dst);
+    logic(vm, dst, src[0] | src[1]);
 }
 
 /*
-**		Comment
+**		Takes two registers / directs / indirects, applies a logical XOR
+**      between the two, and stores the result in the third parameter that is a
+**      register. Modifies the carry.
 */
 
 void	op_xor(void *vm_ptr)
 {
-    vm_ptr += 0;
-    return ;
+    t_vm            *vm;
+    t_process		*proc;
+    t_args			*args;
+    unsigned int    src[2];
+    unsigned int    dst;
+
+    vm = (t_vm *)vm_ptr;
+    proc = vm->procs.cur;
+    args = &vm->procs.cur->args;
+    ft_bzero(&src, sizeof(src));
+    if (args->first->type == REG_CODE)
+        ft_memcpy(&src[0], &(proc->reg[args->first->val]), REG_SIZE);
+    else
+        src[0] = args->first->val;
+    if (args->first->next->type == REG_CODE)
+        ft_memcpy(&src[1], &(proc->reg[args->first->next->val]), REG_SIZE);
+    else
+        src[1] = args->first->next->val;
+    dst = args->first->next->next->val;
+    if (vm->verbose & V_OPERATIONS)
+        vm->print("and %u, %u, %u | ", src[0], src[1], dst);
+    logic(vm, dst, src[0] ^ src[1]);
 }
