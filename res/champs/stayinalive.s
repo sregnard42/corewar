fc5f9cb6c7ee3f09a0cdced3fcca4d02d90551f2
Kinstelle <christelle.mouele@gmail.com>
2020-01-12 19:13:42 +0100

    More champions for tests

--- res/champs/stayinalive.s
+++ res/champs/stayinalive.s
@@ -2,8 +2,13 @@
 .comment "And we're stayin' alive, stayin' alive"
 
 sti r1, %:live, %1
+sti r1, %:main, %1
+
+main:   live %1
+        fork %:live
+        ld %0, r2
+        zjmp %:main
 
 live:   live %1
         ld %0, r2
         zjmp %:live
-        
